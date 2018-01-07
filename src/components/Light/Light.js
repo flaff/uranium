0974c411ade9f63ef191dfae1014cad6715134ad
flaff <flaffy90@gmail.com>
2018-01-07 17:59:53 +0100

    light time format

--- src/components/Light/Light.js
+++ src/components/Light/Light.js
@@ -5,34 +5,15 @@ import moment from 'moment';
 import {Row, Col} from '../Grid/index';
 import Icon from '../Icon/Icon';
 
-const onlyTime = moment => moment.year(1970).month(0).date(0).milliseconds(0);
 const TIME_FORMAT = 'HH[h]mm[m]';
 
-const getTime = (from, to) => {
-    const
-        now = moment(),
-
-        timeFrom = onlyTime(from),
-        timeTo = onlyTime(to),
-        timeNow = onlyTime(now);
-
-    if (timeNow => timeFrom && timeNow <= timeTo) {
-        return moment(timeTo - timeNow).format(TIME_FORMAT);
-    }
-    if (timeNow < timeFrom) {
-        return moment(timeFrom - timeNow).format(TIME_FORMAT);
-    }
-    if (timeNow > timeTo) {
-        return moment(timeFrom + (timeNow.endOf('day') - timeNow)).format(TIME_FORMAT);
-    }
-};
 
 const Light = (props) => {
     return (
         <div className={`${styles.light} ${props.value ? styles.on : ''} ${props.fetching ? 'fetching' : ''}`}
              onClick={() => props.onLightClick({id: props.id, value: !props.value})}>
             <Row>
-                <Col sm={3} />
+                <Col sm={3} className={styles.id}>{props.id}</Col>
                 <Col sm={6}>
                     <Icon name={'light'} className={styles.bulb} />
                 </Col>
@@ -44,13 +25,11 @@ const Light = (props) => {
             </Row>
 
             <Row className={'ta-c'}>
-                <Col sm={6} className={styles.statusIcon}><Icon name={'ticket'} />&nbsp;{props.id}</Col>
-
                 {props.temperature &&
-                    (<Col sm={6} className={styles.statusIcon}><Icon name={'sun'} />&nbsp;{props.temperature}</Col>)}
+                    (<Col sm={12}><Icon name={'sun'} className={styles.statusIcon} />&nbsp;{props.temperature}</Col>)}
 
                 {(props.timeToNextCycle) &&
-                (<Col sm={12} className={styles.statusIcon}><Icon name={'clock'} />&nbsp;<span>{moment.utc(props.timeToNextCycle).format('HH:mm')}</span></Col>)}
+                (<Col sm={12}><Icon name={'stopwatch'} className={styles.statusIcon} />&nbsp;<span>{moment.utc(props.timeToNextCycle).format(TIME_FORMAT)}</span></Col>)}
             </Row>
         </div>
     )
